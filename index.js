e876ea56e6ec3df14e5c78b5f7f75babd90bf937
abhijeet-gite <abhijeetgite2003@gmail.com>
2025-07-26 18:37:59 +0530

    Added health check route for Render

--- index.js
+++ index.js
@@ -36,10 +36,15 @@ app.get('/', (req, res) => {
   res.send('Blogging Platform API is running...');
 });
 
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 // Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
 
 
